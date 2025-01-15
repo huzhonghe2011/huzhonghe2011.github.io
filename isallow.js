5c816c689fd5b35088cccc40da678e12eaff6645
huzhonghe2011 <huzhonghe2@outlook.com>
2025-01-15 12:29:41 +0800

    Update isallow.js

--- isallow.js
+++ isallow.js
@@ -1,4 +1,4 @@
-var isallow = 1;
+var isallow = 0;
 
 var allowedDomains = [
     "https://huzhonghe2011.github.io",
@@ -6,13 +6,11 @@ var allowedDomains = [
     "https://main.qwert114.us.kg"
 ];
 
-
 var referrer = document.referrer;
 
 if (isallow === 0 && !allowedDomains.some(function(domain) {
     return referrer.startsWith(domain);
 })) {
     alert("原作者拒绝加载");
-  
     window.location.href = "about:blank";
 }
