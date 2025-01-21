81ef4aac9576638490ccc7783a7f9761753b3bb8
huzhonghe2011 <huzhonghe2@outlook.com>
2025-01-21 12:40:16 -0800

    Update isallow.js

--- isallow.js
+++ isallow.js
@@ -1,7 +1,7 @@
 var isallow = 0;
 
 var allowedDomains = [
-    "https://huzhonghe2011.github.io/",
+    "https://huzhonghe2011.github.io/isallow.js",
     "https://huzhonghe2011.pages.dev/",
     "https://main.qwert114.us.kg/"
 ];
